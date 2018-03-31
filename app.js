983f3bc0f84f213d31597db15f34bff86bfbf030
Artem <expertum.ave@gmail.com>
2018-03-31 10:44:29 +0300

    made all edits on lesson 6

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const url = require('url');
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 const middleware = require('./middleware')(app, express);
 const config = require('./config');
@@ -11,12 +10,11 @@ Task = require('./models/tasks');
 
 mongoose.connect(config.mongoUri);
 const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {});
 
     app.get('/', (request, response) => { 
         const title = 'List of Tasks'
-        //console.log(listTasks);
         console.log('List Tasks ...');
         Task.find().then((listTask) => {
             response.render('index',{
@@ -27,15 +25,11 @@ const db = mongoose.connection;
     });
     
     app.get('/edittask', (req, res) => {
-        const url_parts = url.parse(req.url, true); 
-        const query = url_parts.query;
-        const id = req.query.id;
-        const name = req.query.name;
+        const { name } = req.query;
         console.log('Edit Task => ', name);
-        const title = 'Edit Task by name: '+name;
+        const title = 'Edit Task by name: ' + name;
         Task.find({name: name}).then((editTask) => {
-            //console.log(editTask[0].name);
-            res.render('edit',{
+            res.render('edit', {
                 title: title,
                 editT: editTask[0]
             });
@@ -43,49 +37,38 @@ const db = mongoose.connection;
     });
 
     app.post('/savechanged', urlencodedParser, (req, res) => {
-        const id = req.body.id;
-        const name = req.body.editname;
-        const text = req.body.edittext;
+        const { id, 'editname': name, 'edittext': text } = req.body;
         console.log('Save changed to => ', name);
-        const title = 'List of Tasks, task '+name+' successfully changed!';
-        Task.findById(id, function (err, doc) {
+        Task.findById(id, (err, doc) => {
             doc.text = text;
             doc.name = name;
-            doc.save();
-        }).then((scs) => {
-            res.redirect('/');
+            doc.save().then(() => {
+                res.redirect('/');
+            });
         });
     });
 
     app.get('/newtask', (req, res) => {
         console.log('Add new task!');
         const title = 'Add new task:';
-        res.render('new',{
+        res.render('new', {
             title: title,
         });
     });
 
     app.post('/createtask', urlencodedParser, (req, res) => {
-        const name = req.body.newname;
-        const text = req.body.newtext;
+        const { 'newname': name, 'newtext': text} = req.body;
         var task = new Task({ name: name, text:text });
-        task.save(function (err) {
+        task.save( (err) => {
             if (err) return handleError(err);
             res.redirect('/');
           })
     });
 
     app.get('/deletetask', (req, res) => {
-        const url_parts = url.parse(req.url, true); 
-        const query = url_parts.query;
-        const id = req.query.id;
-        const name = req.query.name;
+        const { id, name } = req.query;
         console.log('Delete Task => ',name);
-        Task.findById(id, function (err, doc) {
-            doc.remove();
-        }).then((scs) => {
-            res.redirect('/');
-        });          
+        Task.remove({ _id: id }).then(() => res.redirect('/'));         
     });
 
     app.listen(config.port);
--- middleware/index.js
+++ middleware/index.js
@@ -1,25 +1,15 @@
-module.exports = function (app, express) {
+module.exports = (app, express) => {
     const ejs = require('ejs-locals');
     const path = require('path');
     const cookieParser = require('cookie-parser');
   
-    /**
-     * Page Rendering
-     * */
     app.engine('html', ejs);
     app.engine('ejs', ejs);
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'ejs');
 
-    /**
-     * Session
-     * */
      app.use(cookieParser());
 
-     /**
-     * Public directory
-     * */
     app.use(express.static(path.join(__dirname, '../public')));
-    app.use("/public", express.static(path.join(__dirname, '../public')));
-
+    app.use('/public', express.static(path.join(__dirname, '../public')));
 };
